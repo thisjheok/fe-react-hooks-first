44a3cdbc51a7d8c51ff8f65f84bea0b54f2df8cd
thisjheok <jhjang3344@gmail.com>
2024-04-10 20:11:23 +0900

    완

--- src/components/Counter2.jsx
+++ src/components/Counter2.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
-
+import { useState,useEffect } from 'react'
 const Counter2 = () => {
   // 이벤트 핸들러부터 state까지 모두 스스로 작성해보시길 바랍니다!
   // 과연 어떤 state를 생성해주어야하고, 또 어떤 이벤트 핸들러가 필요할까요?
   // useState와 useEffect를 둘다 사용해주셔서 진행해주시면 됩니다!
+  const [count,setCount] = useState(0);
+  const [text,setText] = useState('');
+
+  const Increase = ()=>{
+    setCount(count+1);
+  }
+  const updateText = (e)=>{
+    setText(e.target.value);
+  }
+
+   useEffect(()=>{
+    console.log('렌더링이 완료되었습니다.');
+   },[]) 
+   
+   useEffect(()=>{
+    console.log('Count 값이 변경되었습니다.');
+   },[count]) 
+
+   useEffect(()=>{
+    console.log('text 값이 변경되었습니다.');
+   },[text]) 
 
   return (
     <div className="counter">
       <h>Counter 2</h>
       <hr></hr>
-      <h3>{/*카운트한 숫자가 들어가요*/}</h3>
-      <button>+1</button>
+      <h3>{count}</h3>
+      <button onClick={Increase}>+1</button>
       <hr></hr>
-      <input type={'text'}></input>
-      <h3>{/*입력한 글자가 들어가요*/}</h3>
+      <input type={'text'} onChange={updateText}></input>
+      <h3>{text}</h3>
     </div>
   )
 }
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Name = () => {
+  const [name,setName] = useState('');
+  const [nickName,setnickName] = useState('');
+
+  const updateName = (e)=>{
+    setName(e.target.value);
+  }
+
+  const updateNickName = (e)=>{
+    setnickName(e.target.value);
+  }
+
   return (
     <div className="name">
       <h>Name</h>
       <hr></hr>
-      <input name="myName" placeholder="이름을 입력해주세요" />
-      <input name="nickName" placeholder="닉네임을 입력해주세요" />
-      <p>이름</p>
-      <p>닉네임</p>
+      <input name="myName" placeholder="이름을 입력해주세요" onChange={updateName}/>
+      <input name="nickName" placeholder="닉네임을 입력해주세요" onChange={updateNickName}/>
+      <p>이름 {name}</p>
+      <p>닉네임 {nickName}</p>
     </div>
   )
 }
